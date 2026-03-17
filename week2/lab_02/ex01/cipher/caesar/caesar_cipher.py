b37224d374bb2806d6b1edbf8d1f74f93d586fe4
TranDuyTan <ttan73132@gmail.com>
2026-03-17 09:47:01 +0700

    [add] week2 web

--- week2/lab_02/ex01/cipher/caesar/caesar_cipher.py
+++ week2/lab_02/ex01/cipher/caesar/caesar_cipher.py
@@ -1,4 +1,5 @@
-from cipher.caesar.alphabet import ALPHABET
+from .alphabet import ALPHABET
+
 
 class CaesarCipher:
     def __init__(self):
@@ -8,15 +9,15 @@ class CaesarCipher:
     def encrypt_text(self, text: str, key: int) -> str:
         encrypted_text = []
         try:
-            key = int(key)  # Đảm bảo key là số nguyên
+            key = int(key)  
             for letter in text:
-                if letter.upper() in self.alphabet:  # Kiểm tra chữ hoa trong bảng chữ cái
+                if letter.upper() in self.alphabet:  
                     letter_index = self.alphabet.index(letter.upper())
                     output_index = (letter_index + key) % self.alphabet_len
                     output_letter = self.alphabet[output_index]
                     encrypted_text.append(output_letter if letter.isupper() else output_letter.lower())
                 else:
-                    encrypted_text.append(letter)  # Giữ nguyên dấu câu và khoảng trắng
+                    encrypted_text.append(letter) 
         except ValueError as e:
             return f"Lỗi: {e}"
         return "".join(encrypted_text)
@@ -24,7 +25,7 @@ class CaesarCipher:
     def decrypt_text(self, text: str, key: int) -> str:
         decrypted_text = []
         try:
-            key = int(key)  # Đảm bảo key là số nguyên
+            key = int(key) 
             for letter in text:
                 if letter.upper() in self.alphabet:
                     letter_index = self.alphabet.index(letter.upper())
